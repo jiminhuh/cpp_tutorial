0ae1060ce5213536a254cb063081db36663eb61f
Jimin Huh <jiminhuh95@gmail.com>
2020-06-27 17:03:32 -0500

    cout and cin practice

---
+++ FirstProgram/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    int favorite_number;
+    
+    std::cout << "What is your favorite Number between 1 and 100?";
+
+    std::cin >> favorite_number;
+
+    std::cout << "Wow that is my favorite number too!" << std::endl;
+    return 0;
+}
