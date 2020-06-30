d6bd8cc2d0d58fc5b317f3808db1f3954069919b
Jimin Huh <jiminhuh95@gmail.com>
2020-06-30 01:34:13 -0500

    arrays practice. initializing and looping through using for loop.

---
+++ arrays/arrays.cpp
@@ -0,0 +1,15 @@
+//
+// Created by Jimin Huh on 6/30/20.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string name_array[3] = {"Jimin", "Andy", "Winston"};
+
+    for (int i = 0; i < 3; i++) {
+        cout << name_array[i] << endl;
+    }
+}
---
+++ arrays/arrays.h
@@ -0,0 +1,14 @@
+//
+// Created by Jimin Huh on 6/30/20.
+//
+
+#ifndef CPP_TUTORIAL_ARRAYS_H
+#define CPP_TUTORIAL_ARRAYS_H
+
+
+class arrays {
+
+};
+
+
+#endif //CPP_TUTORIAL_ARRAYS_H
